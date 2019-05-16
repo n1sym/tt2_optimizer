cd685899989705de2ddd38f317255d22c2cd0b3f
EC2 Default User <ec2-user@ip-172-31-46-97.us-east-2.compute.internal>
2019-05-16 13:39:22 +0000

    return hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/cards'
-  get  'static_pages/raidrank'
+ root 'application#hello'
 
 end
