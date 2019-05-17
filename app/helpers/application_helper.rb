ec7d2a9f5847e5e4155372dc599779e6c7f5c83b
EC2 Default User <ec2-user@ip-172-31-46-97.us-east-2.compute.internal>
2019-05-17 10:43:40 +0000

    TT2翻訳をタイトル表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+   # ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')
+    base_title = "TT2翻訳"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+  
 end
