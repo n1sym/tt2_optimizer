7f03e9e28feb6c41d270be05b3dcf0f3cd83dc14
EC2 Default User <ec2-user@ip-172-31-46-97.us-east-2.compute.internal>
2019-10-25 05:12:47 +0000

    1025_2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,12 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
+//= require clipboard
+
+$(document).ready(function(){  
+  
+  var clipboard = new Clipboard('.clipboard-btn');
+  console.log(clipboard);
+	
+});
 
